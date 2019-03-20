d7c11147134d88c6138c805c13e2ee4dab024062
Jason Axelson <jason@hobnob.io>
2019-03-20 11:01:40 -1000

    Fix tests

--- test/timber_ecto_test.exs
+++ test/timber_ecto_test.exs
@@ -15,12 +15,28 @@ defmodule Timber.EctoTest do
           Timber.Ecto.handle_event(
             [:app, :repo, :query],
             1_000,
-            %{query: query, query_time: timer},
+            %{query: query, query_time: timer, params: []},
             []
           )
         end)
 
       assert log =~ "Processed SELECT * FROM table in #{timer}ms"
+    end
+
+    test "prints params" do
+      query = "SELECT * FROM table WHERE id = $1"
+      timer = 0
+      log =
+        capture_log(fn ->
+          Timber.Ecto.handle_event(
+            [:app, :repo, :query],
+            1_000,
+            %{query: query, query_time: timer, params: [42]},
+            []
+          )
+        end)
+
+      assert log =~ "Processed SELECT * FROM table WHERE id = $1 in 0ms with args [42]"
     end
 
     test "query_time is nil" do
